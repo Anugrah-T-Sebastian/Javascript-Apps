1e8c23d6654852660ca15ee390708223c38f9660
Anugrah-T-Sebastian <anugrahsebastian98@gmail.com>
2022-02-17 23:12:24 +0530

    Code updated

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -1,27 +1,50 @@
-let myLeads = ["ww", "le", "pizza"];
+let myLeads = [];
 
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEL = document.getElementById("ul-el");
 const pEl = document.getElementById("p-el");
-const hEl = document.getElementById("h-el")
+const hEl = document.getElementById("h-el");
+
+localStorage.setItem("myLeads", "www.examplelead.com");
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    console.log(inputEl.value);
-    console.log(myLeads);
-    // for(let i = 0; i < myLeads.length; i += 1) {
-    //     ulEL.textContent += myLeads[i];
-    //     pEl.textContent += myLeads[i];
-    // }
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads();
+
+    console.log(localStorage.getItem("myLeads"));
 })
 
-function displayList() {
-    for(let i = 0; i < myLeads.length; i++) {
-        ulEL.innerHTML += "<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+    let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+    let listItems = "";
+    for(let i = 0; i < leadsFromLocalStorage.length; i++) {
+        // listItems += "<li><a target='_blank' href='" + myLeads[i] +"'>" + myLeads[i] + "</a></li>"
+        listItems += `
+                        <li>
+                            <a target="_blank" href="${ leadsFromLocalStorage[i]}">${ leadsFromLocalStorage[i] }</a>
+                        </li>
+                    `
     }
+    ulEL.innerHTML = listItems;
 }
 
-for(let i = 0; i < myLeads.length; i++) {
-    ulEL.innerHTML += "<li>" + myLeads[i] + "</li>";
-}
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------*/
+// for(let i = 0; i < myLeads.length; i++) {           //Code 1.1
+//     ulEL.innerHTML += "<li>" + myLeads[i] + "</li>";
+// }
+
+
+// for(let i = 0; i < myLeads.length; i++) {           //Code 1.2: Same as 1.1 with different method
+//     //Above code can be broken down into 1)Create element 2)set text content 3)append to ul
+//     const li = document.createElement("li");
+//     li.textContent = myLeads[i];
+//     ulEL.append(li);
+// }
+/*--------------------------------------------------------------------------------------------------------------*/
--- javascript function.js
+++ javascript function.js
@@ -17,7 +17,28 @@ inputBtn.addEventListener("click", function() {
 5. inputEl.value :To get value from an input field. The inputEl contains the input HTML object {const inputEl = document.getElementById("input-el");}
 
 6. ulEL.innerHTML   :All the string in the .innerHTML is treated as HTML
-for(let i = 0; i < myLeads.length; i++) {
+for(let i = 0; i < myLeads.length; i++) {           //Code 1.1
     ulEL.innerHTML += "<li>" + myLeads[i] + "</li>";
 }
+
+7. document.createElement() :Used to create DOM elements
+// for(let i = 0; i < myLeads.length; i++) {           //Code 1.2: Same as 1.1 with different method
+//     //Above code can be broken down into 1)Create element 2)set text content 3)append to ul
+//     const li = document.createElement("li");
+//     li.textContent = myLeads[i];
+//     ulEL.append(li);
+// }
+
+8. JSON.parse()     :Turns a string into an array
+let myLeads = `["abc"]`;
+myLeads = JSON.parse(myLeads)
+
+9. JSON.stringyfy()     :Turns an array into a string
+myLeads = JSON.stringify(myLeads);
+
+10. localStorage.setItem()      :To store a string against a key value
+localStorage.setItem("myLeadsKey", JSON.stringify(myLeads));
+
+11. localStorage.getItem()      :To get the string value store against a key
+localStorage.getItem("myLeadsKey")
 */
